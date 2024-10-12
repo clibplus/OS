1a3c27a62fa2d5b4117b8aedc1328e4805568621
algo1337 <bangod1337@gmail.com>
2024-10-12 17:40:29 +0000

    Documentation comments

--- utils.h
+++ utils.h
@@ -1,3 +1,8 @@
 #pragma once
 
-char *ExecuteCmd(const char *data);
+
+//
+//			| -> Execute a command on the system and return response content
+//			| -> Returns command response content upon success or NULL upon failure
+//
+char 		*ExecuteCmd(const char *data);
