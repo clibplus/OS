9ae73b1543aba162d1ca18f95118ca3e9890df45
algo1337 <bangod1337@gmail.com>
2024-10-12 17:38:52 +0000

    ExecuteCmd() Utils.c

---
+++ utils.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *ExecuteCmd(const char *cmd) {
+	if(!cmd)
+		return NULL;
+
+	FILE *fd = popen(cmd, "r");
+	if(!fd)
+		return NULL;
+
+	int BYTES = 0, idx = 0;
+	char BUFFER[1024];
+	char *data = (char *)malloc(1);
+	while(fgets(BUFFER, 1023, fd) != NULL) {
+		idx += strlen(BUFFER);
+		data = (char *)realloc(data, idx);
+		strncat(data, BUFFER, strlen(BUFFER));
+		memset(BUFFER, '\0', 1023);
+	}
+
+	data[idx - 1] = '\0';
+	return data;
+}
---
+++ utils.h
@@ -0,0 +1,3 @@
+#pragma once
+
+char *ExecuteCmd(const char *data);
